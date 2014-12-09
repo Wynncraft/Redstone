3a95ab216224d2637ae4fca264343066a987740d
Jodie Belgrave <rmb1993@gmail.com>
2014-12-09 14:58:18 -0500

    maven stuffs

---
+++ src/main/java/io/minestack/redstone/Redstone.java
@@ -0,0 +1,16 @@
+package io.minestack.redstone;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class Redstone {
+
+    public static void main(String[] args) {
+        new Redstone();
+    }
+
+    public Redstone() {
+        log.info("Started Redstone - Minestack Controller");
+    }
+
+}
